97ba37db32144c1b45746b28aa29ca5eae5390f2
thiago.alvees <thiago.alves0302@gmail.com>
2024-02-02 21:21:52 -0300

    fazendo conexões de forma dinâmica

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import quizz_questions from "../../../assets/data/quizz_questions.json"
 @Component({
   selector: 'app-quizz',
   standalone: true,
@@ -8,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrl: './quizz.component.css'
 })
 export class QuizzComponent {
+  title:string = ""
+  questions:any 
+  questSelect:any 
+  answers:string[] = []
+  answerSelect:string = ""
+  questionIndex:number = 0
+  questionMaxIndex:number = 0
+  finished:boolean = false
+
+  constructor(){}
+
+  ngOnInit():void{
+    if(quizz_questions){
+      this.finished = false
+      this.title = quizz_questions.title
+
+      this.questions = quizz_questions.questions
+      this.questSelect = this.questions[this.questionIndex]
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+    }
+  }
 
 }
